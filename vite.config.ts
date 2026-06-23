6913d2d94c6f724f82f26b32e6457bf9aa1d72e7
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2026-06-23 18:28:46 +0100

    Set Pages asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import swiftWasm from "@elementary-swift/vite-plugin-swift-wasm";
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === "true" ? "/hello-world-swift/" : "/",
   plugins: [swiftWasm()],
 });
